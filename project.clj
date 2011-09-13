bed88713b12a8747ec4fb3a6b057f04015788021
Paul Ingles <paul@oobaloo.co.uk>
2011-09-13 17:43:22 +0100

    Add description + update version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clj-hbase "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject clj-hbase "0.0.1-SNAPSHOT"
+  :description "HBase client for Clojure."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.apache.hbase/hbase "0.90.3-cdh3u1"]]
   :repositories {"cdh.snapshots.repo" "https://repository.cloudera.com/content/repositories/releases/"}
